435d95eec644f6e9fae1767dafcbedae1234ef5d
HOME <karolaj95@gmail.com>
2022-02-27 10:50:56 +0100

    zadanie1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	animal dog = new Animal*();
+    dog.species = "canis";
+    dog.alive = true;
+    dog.name = "Szarik"
+            System.out.println("pies nazywa sie" + dog.name)
+    }
+}
---
+++ src/com/company/animal.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class animal {
+    String species;
+    string name;
+    integer age;
+    double weight;
+    boolean aive;
+
+
+}
